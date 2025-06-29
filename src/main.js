118908b992cf834ce962d25d62d8846dac37173d
Alp <alp@collectivat.cat>
2025-06-29 13:29:46 +0200

    works on local

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import VueI18n from 'vue-i18n'
+// import VueI18n from 'vue-i18n'
 import i18n from './i18n'
 
 const app = createApp(App).use(i18n)
 
-app.use(VueI18n)
+// app.use(VueI18n)
 
 app.use(VueAxios, axios)
 
